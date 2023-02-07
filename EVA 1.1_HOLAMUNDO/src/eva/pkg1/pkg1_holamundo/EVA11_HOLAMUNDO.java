68043d07e1f29c4b95e7c5a29be42a52f3d0abb8
Dirceuu <124701508+Dirceuu@users.noreply.github.com>
2023-02-07 09:30:54 -0600

    Add files via upload

---
+++ EVA 1.1_HOLAMUNDO/src/eva/pkg1/pkg1_holamundo/EVA11_HOLAMUNDO.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva.pkg1.pkg1_holamundo;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA11_HOLAMUNDO {
+
+    
+    public static void main(String[] args) {
+                    System.out.println("HOLA MUNDO");
+    }
+}
