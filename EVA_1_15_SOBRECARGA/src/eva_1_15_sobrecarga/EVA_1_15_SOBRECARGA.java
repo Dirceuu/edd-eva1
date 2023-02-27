fbc840d6a1e7850d2ece40bce9c69e72da9eeae5
Dirceu <124701508+Dirceuu@users.noreply.github.com>
2023-02-26 23:14:48 -0600

    Add files via upload

---
+++ EVA_1_15_SOBRECARGA/src/eva_1_15_sobrecarga/EVA_1_15_SOBRECARGA.java
@@ -0,0 +1,23 @@
+
+package eva_1_15_sobrecarga;
+
+/**
+ *
+ * @author Alan
+ */
+public class EVA_1_15_SOBRECARGA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            //sobrecarga dar mas funciones a algo.
+            //diferentes println, son 10 en total
+            //para crear sobrecarga de metodos la firma del metodo debe de ser unica.
+        System.out.println("Holanmundo");
+        System.out.println("1231");
+        System.out.println(213);
+        System.out.println(true);
+    }
+    
+}
