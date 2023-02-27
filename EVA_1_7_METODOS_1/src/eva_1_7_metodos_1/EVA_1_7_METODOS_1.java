c1b3c3d83ab39f3597b1fae6491434fb1505f8e0
Dirceu <124701508+Dirceuu@users.noreply.github.com>
2023-02-26 22:55:40 -0600

    Add files via upload

---
+++ EVA_1_7_METODOS_1/src/eva_1_7_metodos_1/EVA_1_7_METODOS_1.java
@@ -0,0 +1,16 @@
+package eva_1_7_metodos_1;
+
+/**
+ @author Alan
+ */
+public class EVA_1_7_METODOS_1 {
+
+    public static void myMethod(){
+        System.out.println("You have called me: My name is my method|!!");
+    }
+ 
+    public static void main(String[] args) {
+        myMethod(); // Invocar al metodo; llamada a la función
+        }
+    
+}
