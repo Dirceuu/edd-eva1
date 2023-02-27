d2f310e97df7ac28b0377d2c5c2348952a1692d0
Dirceu <124701508+Dirceuu@users.noreply.github.com>
2023-02-26 23:01:05 -0600

    Add files via upload

---
+++ EVA_1_9_STACKOVERFLOW/src/eva_1_9_stackoverflow/EVA_1_9_STACKOVERFLOW.java
@@ -0,0 +1,18 @@
+package eva_1_9_stackoverflow;
+
+/**
+ *
+ * @author Alan
+ */
+public class EVA_1_9_STACKOVERFLOW {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        A();
+    }
+    public static void A (){
+        A();
+    }
+}
