7b6360f11ca3029f2733784a776853f9c7aa379e
Dirceu <124701508+Dirceuu@users.noreply.github.com>
2023-02-26 23:03:06 -0600

    Add files via upload

---
+++ EVA_10_PARAMETROS/src/eva_10_parametros/EVA_10_PARAMETROS.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_10_parametros;
+
+/**
+ *
+ * @author Alan
+ */
+public class EVA_10_PARAMETROS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        nPrintln("1234", 13231);
+    }
+    public static void nPrintln(String message, int n){
+        for (int i = 0; i < n; i++)
+            System.out.println(message);            
+        }
+    }
+    
